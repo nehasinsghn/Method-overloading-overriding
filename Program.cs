482aa4d3d6108eedc19c313368b9f5bf17592f33
nehasinsghn <97651021+nehasinsghn@users.noreply.github.com>
2022-07-22 15:49:25 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    // Method Overloading
+    class A
+    {
+        public void show()
+        {
+            Console.WriteLine("This is Parameteless Method");
+        }
+        public int show(int a)
+        {
+            Console.WriteLine("This is Parameteries Method");
+            return 0;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            obj.show();
+            obj.show(23);
+            Console.ReadLine();
+        }
+    }
+}
